221a17bf2c40e8f53efbc524a2d6e5e8df3c5fb9
Kelvin Fan <kelvinfan001@gmail.com>
2022-04-09 03:15:20 +0000

    more fix bootstrap

--- bin/setup_dev.sh
+++ bin/setup_dev.sh
@@ -12,6 +12,7 @@ echo "=== Bringing up development network ==="
 pushd smart_contract && npm install --unsafe-perm=true --allow-root
 # Bring up `hardhat` development network
 npx hardhat node &
+HARDHAT_NETWORK_PID=$!
 
 echo "=== Deploying smart contract on development network ==="
 # This also compiles the contract.
@@ -19,4 +20,8 @@ CONTRACT_ACCOUNT=$(npx hardhat run scripts/deploy.js --network localhost)
 popd
 
 echo "=== Bringing up client server ==="    
-CONTRACT_ACCOUNT=${CONTRACT_ACCOUNT} docker-compose up
+CONTRACT_ACCOUNT=${CONTRACT_ACCOUNT} docker-compose up &
+sleep 30
+docker-compose down
+kill $HARDHAT_NETWORK_PID
+exit 0
